8a71c87b8089bacfb72dbea6873dad0cb297dfd5
Steve Willsmore <scwillsmore@gmail.com>
2023-01-24 09:38:44 -0500

    Add relationship, add more employees to db.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store
+  has_many :employees
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jeff", last_name: "Hurst", hourly_rate: 75)
+@store2.employees.create(first_name: "Alan", last_name: "Shearer", hourly_rate: 260)
+@store2.employees.create(first_name: "Ellie", last_name: "Williams", hourly_rate: 120)
