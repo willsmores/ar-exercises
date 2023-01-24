30b4b106ff052643b4fac5551421cf7f473ad99b
Steve Willsmore <scwillsmore@gmail.com>
2023-01-24 10:30:59 -0500

    Added validation rules and error output.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,30 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true }
+  validates :hourly_rate, numericality: { in: 40..200 }
+  validates :store_id, presence: true
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+end
+
+puts "Please enter a new Store name:"
+@userInput = $stdin.gets.chomp
+
+@new_store = Store.create(:name => @userInput)
+
+if @new_store.errors.any?
+  @new_store.errors.each do |error|
+    attribute = error.attribute
+    message = error.message
+    puts "#{attribute} #{message}"
+  end
+end
